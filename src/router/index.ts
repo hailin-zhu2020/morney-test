328d603f032eb400b4ddbcb9deafe0fa15abf4d6
Lin <hailin_zhu2018@126.com>
2021-09-10 21:48:41 +0800

    添加 404 组件

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import Money from '@/views/Money.vue'
 import Labels from '@/views/Labels.vue'
 import Statistics from '@/views/Statistics.vue'
-
+import NotFound from '@/views/NotFound.vue'
 
 
 Vue.use(VueRouter)
@@ -26,7 +25,12 @@ const routes = [
     {
         path: '/statistics',
         component: Statistics
-    }
+    },
+    {
+
+        path: '*',// 会匹配所有路径
+        component: NotFound
+    }//地址输错时，渲染404页面
 
 ]
 
