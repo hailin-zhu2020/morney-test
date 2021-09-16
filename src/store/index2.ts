f67db2be32835f0418c27b88afe735991acb8494
Lin <hailin_zhu2018@126.com>
2021-09-16 22:32:04 +0800

    模块化的store 和 删除了models

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,33 +1,10 @@
-//问题1：全局变量太多;解决方案：全部放到到store对象里
-//问题2：严重依赖window；解决方案：放到一个ts文件中并导出
-import recordListModel from '@/models/recordListModel'
-import tagListModel from '@/models/tagListModel'
+import recordStore from '@/store/recordStore'
+import tagStore from '@/store/tagStore'
 
 const store = {
-    //record store
-    recordList: recordListModel.fetch(),
-    createRecord: (record: RecordItem) => {
-        return recordListModel.create(record);
-    },
-    //tag store
-    tagList: tagListModel.fetch(), //数据变成全局变量，使得各页面引用同一个数据
-    findTag(id: string) {
-        return this.tagList.filter(t => t.id === id)[0];
-    },
-    createTag: (name: string) => {
-        const message = tagListModel.create(name);
-        //提示用户成功或者异常原因
-        if (message === 'duplicated') {
-            window.alert('标签重复！');
-        } else if (message === 'success') {
-            window.alert('添加成功!');
-        }
-    },
-    removeTag: (id: string) => {
-        return tagListModel.remove(id);
-    },
-    updateTag: (id: string, name: string) => {
-        return tagListModel.update(id, name);
-    },
+    ...recordStore, //浅拷贝，只复制地址
+    ...tagStore,
+
 };
+
 export default store;
---
+++ src/store/recordStore.ts
@@ -0,0 +1,24 @@
+//model代码全部放到这了
+import clone from '@/lib/clone'
+
+const localStorageKeyName = 'recordList';
+
+const recordStore = {
+    //record store
+    recordList: [] as RecordItem[],
+    fetchRecords() {
+        this.recordList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];//强制返回值类型
+        return this.recordList;
+    },//获取数据
+    saveRecords() {
+        window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.recordList));
+    },//保存数据
+    createRecord(record: RecordItem) {
+        const record2: RecordItem = clone(record);
+        record2.createAt = new Date();
+        this.recordList && this.recordList.push(record2);//也可用可选链语法：this.recordList?.push(record2);非常新
+        recordStore.saveRecords();
+    },
+}
+recordStore.fetchRecords();
+export default recordStore;
