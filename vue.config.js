2c82d037ea9f940d4af79038bcab158043d84027
Lin <hailin_zhu2018@126.com>
2021-09-21 12:35:06 +0800

    保证用户用手机打开页面，否则就提示用户

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,19 @@
 /* eslint-disable */
 const path = require('path')
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/morney-website/'
+      : '/',
   lintOnSave: false,
   /*用来配置svg*/
-  chainWebpack : config =>{
-    const dir = path.resolve(__dirname,'src/assets/icons')
+  chainWebpack: config => {
+    const dir = path.resolve(__dirname, 'src/assets/icons')
     config.module
         .rule('svg-sprite') //规则名
         .test(/\.svg$/) //符合正则的文件使用该规则
         .include.add(dir).end()//只包含icons目录
         .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod') //新版不会出现scss文件导入不认识的问题，使用svg-sprite-loader就可以了
-        .options({extract:false})//不要解析成文件
+        .options({extract: false})//不要解析成文件
         .end() //使用svg-sprite-loader执行规则
         .use('svgo-loader').loader('svgo-loader') //svgo为svg优化
         .tap(options=>({...options,plugins:[{removeAttrs:{attrs:'fill'}}]}))//删除svg图片中自带的fill属性
