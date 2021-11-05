ed71efbcc06cacc8b744c55dfc53da1ceaad0856
Lin <hailin_zhu2018@126.com>
2021-11-05 21:37:42 +0800

    基本完成chart

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -100,11 +100,6 @@ export default class Statistics extends Vue {
         },
         axisLine: {
           color: '#666'
-        },
-        axisLabel: {
-          formatter: function (value: string, index: number) {
-            return value.substr(5);
-          }
         }
 
       },
